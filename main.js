444379853e2ab8763dacfb9f89fe05b1cdd30a62
sangbate258 <sangbate258@gmail.com>
2025-08-21 14:48:43 +0700

    Update main.js

--- main.js
+++ main.js
@@ -1,28 +1,28 @@
-// const person = {
-//     firstName: 'Sang',
-//     lastName: 'Dương Bá',
-//     age: 21,
-//     isStudent: true,
-//     sayHello: () => console.log("Hi, I'm Sang"),
-//     eat: ()=> console.log("I'm eating chicken")
-// }
+const person = {
+    firstName: 'Sang',
+    lastName: 'Dương Bá',
+    age: 21,
+    isStudent: true,
+    sayHello: () => console.log("Hi, I'm Sang"),
+    eat: ()=> console.log("I'm eating chicken")
+}
 
-// const person2 = {
-//     firstName: 'Anh',
-//     lastName: 'Nguyen Tuan',
-//     age: 21,
-//     isStudent: true,
-//     sayHello: ()=> console.log("Hello, I'm Anh"),
-//     eat: ()=> console.log("I'm eating vegetables"),
-// }
-// console.log(person.lastName, person.firstName, person.age);
-// person.sayHello(); person.eat();
-// console.log(person2.lastName, person2.firstName, person2.age);
-// person.sayHello(); person2.eat();
+const person2 = {
+    firstName: 'Anh',
+    lastName: 'Nguyen Tuan',
+    age: 21,
+    isStudent: true,
+    sayHello: ()=> console.log("Hello, I'm Anh"),
+    eat: ()=> console.log("I'm eating vegetables"),
+}
+console.log(person.lastName, person.firstName, person.age);
+person.sayHello(); person.eat();
+console.log(person2.lastName, person2.firstName, person2.age);
+person.sayHello(); person2.eat();
 
 
 //Bài tập
-const person = {
+const person1 = {
     name: 'sang',
     age: 20,
     hobby: 'game'
